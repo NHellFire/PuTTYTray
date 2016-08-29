9d69be3be89b722f84e9642d7a7ad377617bb95d
NHellFire <nathan.renniewaldock@gmail.com>
2016-08-29 15:01:43 +0100

    Add full Windows build script

---
+++ build.bat
@@ -0,0 +1,9 @@
+@echo off
+cd /D %~dp0
+perl mkfiles.pl
+cd windows
+CALL "%ProgramFiles(x86)%\Microsoft Visual Studio 14.0\Common7\Tools\vsvars32.bat"
+git describe --tags > git_version.txt
+set /p VERSION=<git_version.txt
+del /q git_version.txt
+build-release %VERSION%
